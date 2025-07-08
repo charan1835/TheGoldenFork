2e060c25cf22f93c141962468472d66ab9268855
Charan1835 <chimbilicharan@gmail.com>
2025-07-08 18:24:45 +0530

    feat: initial project code

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 // app/page.js
 import "./globals.css";
-import CategoryList from "./_Components/CategoryList.jsx";
-import MenuItems from "./_Components/Menuitem";
+import CategoryList from "./_components/CategoryList.jsx";
+import MenuItems from "./_components/Menuitem";
 //import CookingGIF from "./_Components/CookingGif";
 //import Hero from "./_Components/Hero";
-import Footer from "./_Components/Footer";
+import Footer from "./_components/Footer";
 export default async function Page() {
 
   return (
