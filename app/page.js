22f38d96e48682f9b7d99754a879111e3d16be14
Charan1835 <chimbilicharan@gmail.com>
2025-07-08 18:56:21 +0530

    fix: proper case rename for CartContext.jsx

--- app/page.js
+++ app/page.js
@@ -2,11 +2,12 @@
 import "./globals.css";
 import HomeClientWrapper from "./_components/HomeClientWrapper";
 import Footer from "./_components/Footer";
-
+import Hero from "./_components/Hero";
 export default async function Page() {
   return (
     <>
       <HomeClientWrapper />
+      <Hero />
       <Footer />
     </>
   );
